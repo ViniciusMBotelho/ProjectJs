e6543ffcca33c532873f5b4b38dbb1aca9ba9cb1
Vinicius Macedo <viniciusmcode@gmail.com>
2023-07-28 19:09:31 -0300

    testes com hook de efeito e axios

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,13 @@
 import express from "express" //biblioteca para criação de servidor
 import { login } from "./routes/users/login.js" //rota relacionada ao login
 import { produtosRouter } from "./routes/produtos/produtosRouter.js" //importando rotas relacionados aos produtos
+import cors from "cors"
 
 export const app = express() //define e exporta o objeto principal do servidor para os seus relacionados
 app.use(express.json()) //permite utilizar json nas requisições
+
+const corsOptions = {origin: '*'}
+app.use(cors(corsOptions))
 
 app.use("/produtos", produtosRouter) //define a rota produtos
 
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,8 +1,50 @@
-import Image from "next/image";
+"use client"; //ativar renderização pelo client
 import styles from "./page.module.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Home() {
-  return <main className={styles.main}>
-    
-  </main>;
+  //pagina inicial do site
+  const [produtos, setProdutos] = useState([]); //controlar comportamento do estado produtos, setProdutos serve para alterar o conteudo do estado
+  const [numero, setNumero] = useState(0); //controlar comportamento do estado numero, seNumero serve para alterar o conteudo do estado
+
+  useEffect(() => {
+    // controlar o que acontece ao carregar a pagina
+    const getProduto = async () =>
+      //aerofunction assincrona que força o site a esperar seu conteudo ser carregado
+      await axios // conteudo a ser carregado
+        .get("http://localhost:3001/produtos/listar")
+        .then((response) => setProdutos(response.data)) //depois de carregado mostrar apenas o conteudo data
+        .catch((error) => console.log(error)); //tratamento de erro
+    getProduto(); //chamada da função
+  }, []);
+
+  const atualiza = () => {
+    //mostrar no site a atualização do estado numero
+    setNumero(numero + 1); //incrementa o valor atual em 1 ***************
+  };
+
+  const DivProduto = () =>
+    //criação da div onde o produto ira aparecer no corpo do site
+    produtos.map(
+      (
+        produto //loop para aparecer todo o array ||||| div id={produto.id} oara que cada div tenha uma nomenclatura unica
+      ) => (
+        <div id={produto.id}>
+          <h4>{produto.nome}</h4>
+          <h5>{produto.preco}</h5>
+          <h6>{produto.categoria}</h6>
+        </div>
+      )
+    );
+
+  return (
+    //corpo do site
+    <main className={styles.main}>
+      <h1>Loja algumacoisa</h1>
+      <DivProduto />
+      <h3>{numero}</h3>
+      <button onClick={atualiza}>Aumentar</button>
+    </main>
+  );
 }
